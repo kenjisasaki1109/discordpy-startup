7ebb847cc9ea6c907ffd70a6e8fb74555018a00a
kenjisasaki1109 <74524921+kenjisasaki1109@users.noreply.github.com>
2020-11-18 00:04:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,11 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+    
+@bot.command()
+async def pachi(ctx):
+    await ctx.send('パチンコ！！！')    
 
     
 bot.run(token)
