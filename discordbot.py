0fe337e5c07c843d8500497569bb3b2f48448f1f
kenjisasaki1109 <74524921+kenjisasaki1109@users.noreply.github.com>
2020-11-17 23:58:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,23 +1,25 @@
-#coding:UTF-8
-import os,discord
-from discord.ext import tasks
-from datetime import datetime 
+from discord.ext import commands
+import os
+import traceback
 
-TOKEN = os.environ["DISCORD_BOT_TOKEN"] #トークン
-CHANNEL_ID = os.environ["CHANNEL_ID"] #チャンネルID
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-# 60秒に一回ループ
-@tasks.loop(seconds=30)
-async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now == '09:43':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('おはよう')
 
-#ループ処理実行
-loop.start()
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+    
+    @bot.command()
+async def neko(ctx):
+    await ctx.send('にゃーん')
+
+bot.run(token)
